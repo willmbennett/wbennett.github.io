b7dfd53970a72c4d251824407eda1b3ad36fe44d
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-04-08 09:55:35 -0700

    Updates

---
+++ precache-manifest.f9ccd7314c0cbf7cd26448aa294f00bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "19cd9274751ef36547f3",
+    "url": "/wbennett.github.io/static/css/main.c2a80700.chunk.css"
+  },
+  {
+    "revision": "19cd9274751ef36547f3",
+    "url": "/wbennett.github.io/static/js/main.19cd9274.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/wbennett.github.io/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/wbennett.github.io/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "308cf114324e9305d85a",
+    "url": "/wbennett.github.io/static/js/runtime~main.308cf114.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/wbennett.github.io/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7c42db737402e69e2119a7f45cdb09e4",
+    "url": "/wbennett.github.io/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/wbennett.github.io/precache-manifest.f9ccd7314c0cbf7cd26448aa294f00bc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/wbennett.github.io/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
